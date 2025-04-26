14d99e7de2dc1d10f79d50a0075d868e106c8863
utsab-ad <utasbadhikari075@gmail.com>
2025-04-25 19:30:27 -0600

    Data Types

---
+++ premitiveDataType.js
@@ -0,0 +1,16 @@
+let a = null; // Null
+let b = 10;  // Numbers
+let c= true;  // Boolean ==> True or False
+let d = BigInt("1000"); // BigInt
+let e = "utsab adhikari"; // String
+let f = Symbol("Symbol"); // Symbol
+let g // Undefined
+
+console.log("These are the Premitiv DataTypes:");
+console.log(typeof a, a);
+console.log(typeof b, b);
+console.log(typeof c, c);
+console.log(typeof d, d);
+console.log(typeof e, e);
+console.log(typeof f, f);
+console.log(typeof g, g);
