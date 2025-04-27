2c2279388cdc1b86f56cfe970b70c85a4d3837e7
utsab-ad <utasbadhikari075@gmail.com>
2025-04-27 10:47:07 -0600

    Loops started

---
+++ J01_P03_LoopLearn.js
@@ -0,0 +1,13 @@
+let roll = {
+    utsab: 241254,
+    ram: 241258,
+    hari: 241550,
+    sita: 251460,
+    kim: 267080
+
+}
+
+for(let i=0; i<Object.keys(roll).length; i++)
+        {
+            console.log("The Roll No. of " + Object.keys(roll)[i] + " is " + roll[Object.keys(roll)[i]]);
+        }
