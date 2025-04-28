f66d696c58e7e5953d3b077b0559f08bda388b90
utsab-ad <utasbadhikari075@gmail.com>
2025-04-28 08:15:05 -0600

    updated loop

--- J01_P03_LoopLearn.js
+++ J01_P03_LoopLearn.js
@@ -11,3 +11,33 @@ for(let i=0; i<Object.keys(roll).length; i++)
         {
             console.log("The Roll No. of " + Object.keys(roll)[i] + " is " + roll[Object.keys(roll)[i]]);
         }
+
+for(i=0;i<2;i++)
+        {
+            console.log(1+i);
+        }
+
+let obj = {
+    name: "utsab",
+    role: "programmer",
+    college: "NCIT"
+}
+
+for (const key in obj) {
+
+        const element = obj[key];
+        console.log(key, element)  
+}
+
+for( const char of "Utsab")
+        {
+            console.log(char);
+        }
+
+        let index = 13;
+
+while(index>10)
+    {
+        console.log(index);
+        index--;
+    }
